272a6e685b917c0c333345dd886a3b47ab28d249
KyleKw0k <76418666+KyleKw0k@users.noreply.github.com>
2021-04-20 20:08:32 +0800

    Add files via upload

--- src/main/java/ouhk/comps380f/controller/MenuController.java
+++ src/main/java/ouhk/comps380f/controller/MenuController.java
@@ -1,9 +1,19 @@
 package ouhk.comps380f.controller;
 
 import java.io.IOException;
+import java.security.Principal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,25 +27,26 @@ import org.springframework.web.servlet.view.RedirectView;
 import ouhk.comps380f.model.Attachment;
 import ouhk.comps380f.model.Comment;
 import ouhk.comps380f.model.Food;
+import ouhk.comps380f.database.DatabaseOperation;
 
 @Controller
 @RequestMapping("/menu")
 public class MenuController {
-
-    private volatile long ITEM_ID_SEQUENCE = 1;
-    private Map<Long, Food> menuDatabase = new Hashtable<>();
+    DatabaseOperation dbOperation = new DatabaseOperation();
+    private Map<String, Food> menuDatabase = new Hashtable<>();
+    //private Map<String, Comment> commentDatabase = new Hashtable<>();
 
     @GetMapping(value = {"", "/list"})
     public String list(ModelMap model) {
         Food item1 = new Food();
-        item1.setId(1);
+        item1.setId("1");
         item1.setFoodName("Hamburger");
         item1.setDescription("A hamburger with cheese.");
         item1.setPrice(20);
         item1.setAvailability(true);
         
         Food item2 = new Food();
-        item2.setId(2);
+        item2.setId("2");
         item2.setFoodName("Fish and Chips");
         item2.setDescription("A delicious Fish and Chips.");
         item2.setPrice(25.5);
@@ -48,8 +59,48 @@ public class MenuController {
         return "list";
     }
     
+    public static class CommentForm {
+
+        private String name;
+        private String id;
+        private String body;
+        private List<MultipartFile> attachments;
+        
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+        
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getBody() {
+            return body;
+        }
+
+        public void setBody(String body) {
+            this.body = body;
+        }
+
+        public List<MultipartFile> getAttachments() {
+            return attachments;
+        }
+
+        public void setAttachments(List<MultipartFile> attachments) {
+            this.attachments = attachments;
+        }
+    }
+    
     @GetMapping("/view/{foodId}")
-    public String view(@PathVariable("foodId") long foodId,
+    public String view(@PathVariable("foodId") String foodId,
             ModelMap model) {
         Food item = this.menuDatabase.get(foodId);
         if (item == null) {
@@ -57,6 +108,23 @@ public class MenuController {
         }
         model.addAttribute("foodId", foodId);
         model.addAttribute("food", item);
+        model.addAttribute("commentForm", new CommentForm());
+        model.addAttribute("commentDatabase", dbOperation.getCommentList(foodId));
         return "view";
+    }
+    
+    @PostMapping("/view/{foodId}")
+    public String create(CommentForm form, Principal principal) throws IOException {
+        String commentId = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+        commentId = principal.getName() + commentId;
+        Comment comment = new Comment();
+        comment.setCommentId(commentId);
+        comment.setUserName(principal.getName());
+        comment.setContents(form.getBody());
+        comment.setItemId(form.getId());
+        
+        dbOperation.addComment(comment);
+        //this.commentDatabase.put(commentId, comment);
+        return "redirect:/menu/view/" + form.getId();
     }
 }
