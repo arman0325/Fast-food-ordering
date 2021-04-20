6e389f6a6cee04dee54ab2ead9372f8b467f5ba6
KyleKw0k <76418666+KyleKw0k@users.noreply.github.com>
2021-04-20 20:38:39 +0800

    Add files via upload

--- src/main/java/ouhk/comps380f/model/Comment.java
+++ src/main/java/ouhk/comps380f/model/Comment.java
@@ -3,8 +3,26 @@ package ouhk.comps380f.model;
 import java.io.Serializable;
 
 public class Comment implements Serializable {
+    private String commentId;
+    private String itemId;
     private String userName;
     private String contents;
+
+    public String getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(String commentId) {
+        this.commentId = commentId;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
 
     public String getUserName() {
         return userName;
